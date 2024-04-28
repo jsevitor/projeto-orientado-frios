7581e84bf5783d8f447a0f6cb2db5656bbf41cc2
José Vitor Oliveira Reis <vitorjseo@gmail.com>
2024-04-28 20:22:51 -0300

    SideBarMenu edited

--- src/components/SideBarMenu.jsx
+++ src/components/SideBarMenu.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressCard, faArrowRightArrowLeft, faArrowsLeftRightToLine, faCircle, faClipboardList, faGear, faList } from '@fortawesome/free-solid-svg-icons';
+
 import '../styles/components/sideBarMenu.sass'
 
 export default function SideBarMenu() {
@@ -7,21 +10,25 @@ export default function SideBarMenu() {
             <ul className="menu">
                 <li>
                     <a href="">
+                        <FontAwesomeIcon icon={faGear} className="icons" />
                         <h2>Painel de Controle</h2>
                     </a>
                 </li>
                 <li>
                     <a href="">
+                    <FontAwesomeIcon icon={faClipboardList} className="icons" />
                         <h2>Produtos</h2>
                     </a>
                 </li>
                 <li>
                     <a href="">
+                    <FontAwesomeIcon icon={faAddressCard} className="icons" />
                         <h2>Cadastros</h2>
                     </a>
                 </li>
                 <li>
                     <a href="">
+                    <FontAwesomeIcon icon={faArrowRightArrowLeft} className="icons" />
                         <h2>Movimentações</h2>
                     </a>
                 </li>
