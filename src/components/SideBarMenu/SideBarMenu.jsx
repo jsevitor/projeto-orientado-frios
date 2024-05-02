567528cb6781ef208b4a11db1c0d000650eacd6a
voliverx <jose.vitor@estudante.iftm.edu.br>
2024-05-02 13:27:25 -0300

    interactivity implemented

--- src/components/SideBarMenu/SideBarMenu.jsx
+++ src/components/SideBarMenu/SideBarMenu.jsx
@@ -1,56 +1,109 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faAddressCard,
-    faArrowRightArrowLeft,
-    faChevronCircleLeft,
-    faClipboardList,
-    faGear,
-
-} from '@fortawesome/free-solid-svg-icons';
-
+import { useState } from 'react'
 import '../SideBarMenu/sideBarMenu.sass'
+import { useEffect } from 'react';
 
 export default function SideBarMenu() {
+    const [isOpenProdutosSubMenu, setIsOpenProdutosSubMenu] = useState(() => {
+        return localStorage.getItem('isOpenProdutosSubMenu') === 'true';
+    });
+    const [isOpenCadastrosSubMenu, setIsOpenCadastrosSubMenu] = useState(() => {
+        return localStorage.getItem('isOpenCadastrosSubMenu') === 'true';
+    });
+    const [menuCollapsed, setMenuCollapsed] = useState(() => {
+        return localStorage.getItem('menuCollapsed') === 'true';
+    });
+
+    useEffect(() => {
+        localStorage.setItem('isOpenProdutosSubMenu', isOpenProdutosSubMenu);
+        localStorage.setItem('isOpenCadastrosSubMenu', isOpenCadastrosSubMenu);
+        localStorage.setItem('menuCollapsed', menuCollapsed);
+    }, [isOpenProdutosSubMenu, isOpenCadastrosSubMenu, menuCollapsed]);
+
+    // useEffect(() => {
+    //     localStorage.setItem('isOpenCadastrosSubMenu', isOpenCadastrosSubMenu);
+    // }, [isOpenCadastrosSubMenu]);
+
+    // useEffect(() => {
+    //     localStorage.setItem('menuCollapsed', menuCollapsed);
+    // }, [menuCollapsed]);
+
+    const toggleProdutosSubMenu = () => {
+        setIsOpenProdutosSubMenu(!isOpenProdutosSubMenu);
+
+        if (menuCollapsed) {
+            setMenuCollapsed(!menuCollapsed)
+        }
+    };
+
+    const toggleCadastrosSubMenu = () => {
+        setIsOpenCadastrosSubMenu(!isOpenCadastrosSubMenu);
+
+        if (menuCollapsed) {
+            setMenuCollapsed(!menuCollapsed)
+        }
+    };
+
+    const toggleMenu = () => {
+        setMenuCollapsed(!menuCollapsed);
+
+        if (isOpenProdutosSubMenu) {
+            setIsOpenProdutosSubMenu(!isOpenProdutosSubMenu)
+        }
+        
+        if (isOpenCadastrosSubMenu) {
+            setIsOpenCadastrosSubMenu(!isOpenCadastrosSubMenu)
+        }
+    };
 
     return (
-        <nav className="nav_container">
-            <div id="hideMenu">
-                <FontAwesomeIcon icon={faChevronCircleLeft} className="icons" />
+        <nav className={`nav_container ${menuCollapsed ? 'collapsed' : ''}`}>
+            <div id="hideMenu" onClick={toggleMenu}>
+                <i className={`bx ${menuCollapsed ? 'bx-chevron-right-circle' : 'bx-chevron-left-circle'}`}></i>
             </div>
 
             <ul className="menu">
                 <li>
-                    <a href="/">
-                        <FontAwesomeIcon icon={faGear} className="icons" />
-                        <h2>Painel de Controle</h2>
+                    <a href="/" className='menu_option'>
+                        <i class='bx bx-cog'></i>
+                        {!menuCollapsed && <h2>Painel de Controle</h2>}
                     </a>
                 </li>
                 <li>
-                    <a href="/entrada">
-                        <FontAwesomeIcon icon={faClipboardList} className="icons" />
-                        <h2>Produtos</h2>
+                    <a href='#' className='menu_option' onClick={toggleProdutosSubMenu}>
+                        <i class='bx bx-list-ol' ></i>
+                        {!menuCollapsed && <h2>Produtos</h2>}
+                        {!menuCollapsed &&
+                            <i className={`bx ${isOpenCadastrosSubMenu ? 'bx-chevron-up' : 'bx-chevron-down'}`} ></i>
+                        }
                     </a>
-                    <ul className='subMenu'>
-                        <li><a href="/entrada"><h3>Entrada de Produtos</h3></a></li>
-                        <li><a href="/retirada"><h3>Retirada de Produtos</h3></a></li>
-                        <li><a href="/lista"><h3>Lista de Produtos</h3></a></li>
-                    </ul>
+                    {isOpenProdutosSubMenu &&
+                        <ul className={`subMenu ${isOpenProdutosSubMenu ? 'collapsedSubMenu' : ''}`}>
+                            <li><a href="/entrada"><h3>Entrada de Produtos</h3></a></li>
+                            <li><a href="/retirada"><h3>Retirada de Produtos</h3></a></li>
+                            <li><a href="/lista"><h3>Lista de Produtos</h3></a></li>
+                        </ul>
+                    }
                 </li>
                 <li>
-                    <a href="/fornecedor">
-                        <FontAwesomeIcon icon={faAddressCard} className="icons" />
-                        <h2>Cadastros</h2>
+                    <a href='#' className='menu_option' onClick={toggleCadastrosSubMenu}>
+                        <i class='bx bx-id-card' ></i>
+                        {!menuCollapsed && <h2>Cadastros</h2>}
+                        {!menuCollapsed &&
+                            <i className={`bx ${isOpenCadastrosSubMenu ? 'bx-chevron-up' : 'bx-chevron-down'}`} ></i>
+                        }
                     </a>
-                    <ul className='subMenu'>
-                        <li><a href="/fornecedor"><h3>Cadastro de Fornecedor</h3></a></li>
-                        <li><a href="/produtos"><h3>Cadastro de Produtos</h3></a></li>
-                        <li><a href="/usuario"><h3>Cadastro de Usuário</h3></a></li>
-                    </ul>
+                    {isOpenCadastrosSubMenu &&
+                        <ul className={`subMenu ${isOpenCadastrosSubMenu ? 'collapsedSubMenu' : ''}`}>
+                            <li><a href="/fornecedor"><h3>Cadastro de Fornecedor</h3></a></li>
+                            <li><a href="/produtos"><h3>Cadastro de Produtos</h3></a></li>
+                            <li><a href="/usuario"><h3>Cadastro de Usuário</h3></a></li>
+                        </ul>
+                    }
                 </li>
                 <li>
                     <a href="">
-                        <FontAwesomeIcon icon={faArrowRightArrowLeft} className="icons" />
-                        <h2>Movimentações</h2>
+                        <i class='bx bx-transfer' ></i>
+                        {!menuCollapsed && <h2>Movimentações</h2>}
                     </a>
                 </li>
             </ul>
